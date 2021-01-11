2506f573f697f81e612b131f1ebbc079c8d66a56
Quentin Meeus <qmeeus@gmail.com>
2021-01-11 14:35:28 +0100

    custom RNN with LSTM+GRU units for easier parameter search

--- assist/acquisition/lstm.py
+++ assist/acquisition/lstm.py
@@ -3,14 +3,75 @@ from torch import nn
 from torch.nn import functional as F
 from torch.utils.data import DataLoader
 from assist.acquisition.mlp import Classifier as BaseClassifier
+from assist.tools import logger
+
+
+class RNN(nn.Module):
+
+    def __init__(self, input_dim, output_dim, hidden_dim=128, num_layers=1, dropout=.1, rnn_type="lstm"):
+        super(RNN, self).__init__()
+
+        RNNClass = nn.GRU if rnn_type.lower() is "gru" else nn.LSTM
+
+        self.rnn = RNNClass(
+            input_size=input_dim,
+            hidden_size=hidden_dim,
+            num_layers=num_layers,
+            batch_first=True,
+            dropout=0, # if num_layers == 1 else dropout,
+            bidirectional=True
+        )
+        
+        self.dropout = nn.Dropout(dropout)
+        self.fc = nn.Linear(hidden_dim * 2, output_dim)
+
+        self.loss_fct = nn.BCEWithLogitsLoss(reduction="sum")
+
+        self.num_directions = 2
+        self.num_layers = num_layers
+        self.hidden_dim = hidden_dim
+
+
+    def forward(self, features, lengths, labels=None):
+        bs, maxlen, _ = features.size()
+        packed_sequences = nn.utils.rnn.pack_padded_sequence(
+            features, lengths.cpu(), batch_first=True, enforce_sorted=False
+        )
+
+        packed_output, _ = self.rnn(packed_sequences)
+        out_rnn, _ = nn.utils.rnn.pad_packed_sequence(packed_output, batch_first=True)
+        out_rnn = out_rnn.view(bs, maxlen, self.num_directions, self.hidden_dim)
+        hidden = self.dropout(torch.cat((
+            out_rnn[torch.arange(bs), lengths - 1, 0, :],    # forward
+            out_rnn[torch.arange(bs), 0, 1, :]               # backward
+        ), dim=-1))
+
+        assert hidden.size() == (bs, self.hidden_dim * self.num_directions)
+        logits = self.fc(hidden)
+
+        if labels is not None:
+            loss = self.compute_loss(logits, labels)
+            return loss, logits
+
+        return logits
+
+    def compute_loss(self, logits, labels):
+        return self.loss_fct(logits, labels.float())
 
 
 class LSTM(nn.Module):
 
-    def __init__(self, input_dim, output_dim, hidden_dim=128, dropout=0.1):
+    def __init__(self, input_dim, output_dim, hidden_dim=128, num_layers=1, dropout=.1):
         super(LSTM, self).__init__()
 
-        self.lstm = nn.LSTM(input_dim, hidden_dim, batch_first=True, bidirectional=True)
+        self.lstm = nn.LSTM(
+            input_dim, 
+            hidden_dim, 
+            batch_first=True, 
+            bidirectional=True, 
+            dropout=dropout
+        )
+        
         self.dropout = nn.Dropout(dropout)
         self.fc = nn.Linear(hidden_dim * 2, output_dim)
 
@@ -80,13 +141,15 @@ def pad_and_sort_batch(batch, sort=False):
 class Classifier(BaseClassifier):
 
     def build(self):
-        self.device = torch.device(self.config.get("acquisition", "device"))
-        return LSTM(
-            input_dim=int(self.config.get("acquisition", "input_dim")),
+        self.device = torch.device(self.config["device"])
+        return RNN(
+            input_dim=int(self.config["input_dim"]),
             output_dim=self.n_classes,
-            hidden_dim=int(self.config.get("acquisition", "hidden_dim")),
-            dropout=float(self.config.get("acquisition", "dropout"))
-        ).to(self.device)
+            hidden_dim=int(self.config["hidden_dim"]),
+            num_layers=int(self.config["num_layers"]),
+            dropout=float(self.config["dropout"]),
+            rnn_type=self.config.get("rnn_type", "lstm")
+        ).to(self.device)        
 
     def get_dataloader(self, dataset, is_train=True):
         return DataLoader(
